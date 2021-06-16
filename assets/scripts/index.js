471fdcbc477f512c05571f5f9bec55e68ac49cba
RichardKurian <richardkurian18@gmail.com>
2021-06-16 13:13:50 +0530

    hello

---
+++ assets/scripts/index.js
@@ -0,0 +1,18 @@
+
+/*function getInputValue(){
+    let input = event.target.innerText
+    console.log(input);
+    printValue(input);
+}*/
+
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+
